6904dea013e3589993b285a8103b5d45bf32a330
ronavarr <roberto.navarra@everis.com>
2023-01-24 14:55:41 +0100

    initial commit

---
+++ src/main/java/org/acme/CacheResource.java
@@ -0,0 +1,31 @@
+package org.acme;
+
+import io.quarkus.infinispan.client.Remote;
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/cache")
+public class CacheResource {
+
+    @Inject
+    @Remote("simpleCache")
+    RemoteCache<String, String> cache;
+
+    @GET
+    @Path("/{key}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String get(@PathParam("key") String key) {
+        return cache.get(key);
+    }
+
+    @PUT
+    @Path("/{key}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String put(String key, String value) {
+        return cache.put(key, value);
+    }
+}
